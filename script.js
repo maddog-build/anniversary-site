af37f689a8b646498ee5443b3f1b1a15a675f380
Reshmiya k shaji <reshmiyashaji1@gmail.com>
2026-06-08 00:00:43 +0530

    Initial anniversary website

---
+++ script.js
@@ -0,0 +1,29 @@
+function checkPassword() {
+
+    const pass = document.getElementById("password").value;
+
+    if(pass === "10624") {
+
+        document.getElementById("lockScreen").style.display="none";
+        document.getElementById("mainContent").style.display="block";
+
+    } else {
+
+        alert("Wrong Password");
+    }
+}
+const anniversary = new Date("June 10, 2026");
+
+function updateCountdown(){
+
+    const now = new Date();
+
+    const diff = anniversary - now;
+
+    const days = Math.floor(diff/(1000*60*60*24));
+
+    document.getElementById("countdown").innerHTML =
+        days + " days until our anniversary ❤️";
+}
+
+setInterval(updateCountdown,1000);
